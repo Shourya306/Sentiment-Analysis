e178899881d34d4e4338b51a3b4404453fcce92f
Shourya306 <shouryareddy306@gmail.com>
2022-10-09 11:00:24 +0530

    Final verison

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from tkinter import Button
 import streamlit as st
 from textblob import TextBlob
+from cleantext import clean
+
 
 st.title('Sentiment Analysis')
 
-res = st.text_area('How was your day?')
-
-if st.button('Submit'):
-    blob = TextBlob(res)
+def sentiment_analysis(text):
+    cle = clean(text,no_punct=True, lower=False)
+    blob = TextBlob(cle)
     sent = blob.sentiment.polarity
     st.write(f'Sentiment score: {sent}')
     if sent >= 0.5:
-        st.success('Positive')
+         st.success('Positive')
     elif sent <= -0.5:
-        st.error('Negetive')
+         st.error('Negetive')
     else:
-        st.warning('Netural')
+         st.warning('Netural')
+
+st.write(sentiment_analysis(st.text_area('How was your day?')))
+
+txt = st.text_area('Text')
+if st.button('Analysize'):
+    st.write(sentiment_analysis(txt))
+
