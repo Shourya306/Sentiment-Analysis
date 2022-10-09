d85ee6b70cd5b09de8e40aa0624c12720a14ae9f
Shourya306 <shouryareddy306@gmail.com>
2022-10-09 11:19:20 +0530

    Final commit

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ def sentiment_analysis(text):
     cle = clean(text,no_punct=True, lower=False)
     blob = TextBlob(cle)
     sent = blob.sentiment.polarity
-    st.write(f'Sentiment score: {sent}')
     if sent >= 0.5:
          st.success('Positive')
     elif sent <= -0.5:
          st.error('Negetive')
     else:
          st.warning('Netural')
+    st.write(f'Sentiment score: {sent}')
+    
 
 txt = st.text_area('Text')
 if st.button('Analysize'):
